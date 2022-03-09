6e21d91694ed57d861b3455e2e489c0890260694
greyblue9 <>
2022-03-09 14:14:31 +0000

    fix build script

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ classpath="${CLASSPATH}${CLASSPATH:+:}$( find "$( pwd )/lib" -name "*.jar" | tr 
 rm -rf out && mkdir -p out
 find ./src -name "*.java" -exec javac -g -implicit:none -proc:none -nowarn -verbose -cp "./lib/deps.jar:./lib/jackson-core-2.2.3.jar" -d out/ "{}" + && rm -vf -- lib/Ab.jar && ( cd out; zip -Xy ../lib/Ab.jar -r . ; );
 [ $? -eq 0 ] || exit 1
-dx=$( whence -p dx )
+dx=$( which dx )
 if [ -n "$dx" ]; then
   dx --dex --multi-dex --keep-classes --output=/tmp/classes.jar ./lib/Ab.jar && cp -pvf -- /tmp/classes.jar ./lib/Ab.jar
 else
-  ttue
+  true
 fi
 
 
